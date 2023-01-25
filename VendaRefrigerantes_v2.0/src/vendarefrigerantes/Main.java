9046fea45b088a1adb9796f30f1cd22e7fee1004
Simao Moreno <simaojmoreno@gmail.com>
2023-01-25 16:35:24 +0000

    Some refactoring of Main.java functions

--- VendaRefrigerantes_v2.0/src/vendarefrigerantes/Main.java
+++ VendaRefrigerantes_v2.0/src/vendarefrigerantes/Main.java
@@ -34,43 +34,183 @@ public class Main {
         // initializeProdCoins(); // carregar as informações sobre qtdd de moedas e
         // preço dos produtos
 
-        System.out.print("Código\t\tOpção\n"
-                + "--------------------------------------------------------------\n"
-                + "[1]\t\tSelecionar lista de produtos\n"
-                + "[2]\t\tLogin como administrador\n"
-                + "[0]\t\tSAIR DA APLICACAO\n"
-                + "--------------------------------------------------------------\n"
-                + " >> ");
-        int opcao = ler.nextInt();
+        int opcao;
 
-        switch (opcao) {
-            case 1:
-                listarProdutos();
-                break;
+        do {
+            System.out.print("\n\nCódigo\t\tOpção\n"
+                    + "--------------------------------------------------------------\n"
+                    + "[1]\t\tSelecionar lista de produtos\n"
+                    + "[2]\t\tLogin como administrador\n"
+                    + "[0]\t\tSAIR DA APLICACAO\n"
+                    + "--------------------------------------------------------------\n"
+                    + " >> ");
+            opcao = ler.nextInt();
 
-            case 2:
-                menuAdministrador();
-                break;
+            switch (opcao) {
+                case 1:
+                    listarProdutos();
+                    break;
 
-            case 0:
-                System.out.println("Adeus e obrigado! ");
-                System.exit(0);
-                break;
+                case 2:
+                    adminMenu();
+                    break;
 
-            default:
-        }
+                default:
+                    System.out.println("\n\nOPCAO INVALIDA!!!");
+            }
+        } while (opcao != 0);
 
         new Ficheiros().escreverFile(moedas, txtMoedas); // gravar dados no ficheiro
         new Ficheiros().escreverFile(produtos, txtProdutos); // gravar dados no ficheiro
+
+        System.out.println("\n\nADEUS E OBRIGADO! \n\n\n\n");
+
     }
 
-    // adicionar moedas
-    private static void addMoedas(ArrayList<Moedas> moedas) {
+    /**
+     * Imprima a descrição, o preço e a quantidade de cada produto registado na tela
+     */
+    private static void listarProdutos() {
+
+        int s_opcao;
+
+        do {
+            System.out.print("\n\nCódigo\t\tProduto\t\tPreço\t\tQtdd.\n"
+                    + "--------------------------------------------------------------\n");
+
+            for (Produto prod : produtos) {
+                System.out.print("[" + (produtos.indexOf(prod) + 1) + "]");
+                System.out.print("\t\t" + prod.getDescrição());
+                System.out.print("\t\t" + prod.getPreço() + "$00");
+                System.out.print("\t\t" + prod.getQtdade() + " unid.\n");
+            }
+
+            System.out.print("[0]\t\tVOLTAR\n"
+                    + "--------------------------------------------------------------\n"
+                    + " >> ");
+
+            s_opcao = ler.nextInt();
+
+            if (s_opcao != 0 && s_opcao < (produtos.size() + 1))
+                comprar(s_opcao);
+            else
+                System.out.println("\n\nOPCAO INVALIDA!!!");
+
+        } while (s_opcao != 0);
+
+    }
+
+    /**
+     * Processa a operação de compra
+     * @param opcao
+     */
+    private static void comprar(int opcao) {
+
+        Produto produto = new Produto(
+                produtos.get(opcao - 1).getDescrição(),
+                produtos.get(opcao - 1).getPreço(),
+                produtos.get(opcao - 1).getQtdade());
+
+        System.out.println("Produto escolhido: " + produto.getDescrição() + "\nPreço: " + produto.getPreço() + "$00");
+
+        System.out.print("Por favor, Introduza a moeda no valor correspondente na maquina\n >> ");
+        int valor = ler.nextInt();
+
+        while (valor < produto.getPreço()) {
+            System.out.print("Falta mais " + (produto.getPreço() - valor) + "$00"
+                    + "Por favor, Introduza a moeda no valor correspondente na maquina\n>");
+            valor += ler.nextInt();
+        }
+
+        System.out.println("Total inserido: " + valor + "$00");
+
+        if (valor > produto.getPreço()) {
+
+            int diferenca = valor - produto.getPreço();
+
+            System.out.println("\nTroco: " + diferenca + "$00");
+
+            processTroco(diferenca, produto.getPreço());
+        }
+
+        System.out.println("Retire o seu produto! Obrigado");
+
+        produto.setQtdade(produto.getQtdade() - 1);
+        produtos.set(opcao - 1, produto);
+
+        new Ficheiros().escreverFile(produtos, txtProdutos);
+    }
+
+    /**
+     * Atualiza o total de moedas resultantes da devolução de troco
+     * 
+     * @param diferenca total de troco a devolver
+     * @param preço     preço total do produto comprado
+     */
+    private static void processTroco(int diferenca, int preço) {
+
+        do {
+
+            int value = 5; // dado que a moeda de menor valor é 5
+
+            if (diferenca >= 100)
+                value = 100;
+            else if (diferenca >= 50)
+                value = 50;
+            else if (diferenca >= 20)
+                value = 20;
+            else if (diferenca >= 10)
+                value = 10;
+
+            for (Moedas moeda : moedas)
+                if (moeda.getValor() == value) {
+                    moeda.setQtdade(moeda.getQtdade() - 1); // decrementar a quantidade da moeda dado como troco
+                    moedas.set(moedas.indexOf(moeda), moeda); // atualizar a moeda na lista de moedas
+                    diferenca -= value; // incrementar a diferenca de troco restante
+                    break; // nao procurar mais moedas
+                }
+
+        } while (diferenca > preço);
+        
+        new Ficheiros().escreverFile(moedas, txtMoedas);
+    }
+
+    /**
+     * Inicializar as listas de produtos e das moedas com os dados padrão
+     */
+    private static void initialize() {
+
+        // carregar dados dos ficheiros
+        produtos.add(new Produto("Cola", 80, 9));
+        produtos.add(new Produto("Fanta", 70, 7));
+        produtos.add(new Produto("Sprit", 60, 8));
+        produtos.add(new Produto("Sumol", 80, 6));
+
+        moedas.add(new Moedas(5, 10));
+        moedas.add(new Moedas(10, 10));
+        moedas.add(new Moedas(20, 10));
+        moedas.add(new Moedas(50, 10));
+        moedas.add(new Moedas(100, 10));
+
+    }
+
+    /**
+     * Menu do administrador para inserção de novas moedas e produtos
+     */
+    private static void adminMenu() {
+
+    }
+
+    /**
+     * Apresenta o menu e faz a operação de inserção de moedas na maquina
+     */
+    private static void addMoedas() {
 
         boolean valido = true;
 
         int qtdade = 0;
         int valor = 0;
+
         do {
 
             System.out.println("==========Adicionar Moedas============= ");
@@ -101,143 +241,4 @@ public class Main {
         ler.next();
     }
 
-    // menu produtos
-    private static void listarProdutos() {
-
-        int i = 0;
-
-        System.out.print("\n\nCódigo\t\tProduto\t\tPreço\t\tQuantidade\n"
-                + "--------------------------------------------------------------\n");
-
-        for (Produto prod : produtos) {
-            System.out.print("[" + ++i + "]");
-            System.out.print("\t\t" + prod.getDescrição());
-            System.out.print("\t\t" + prod.getPreço() + "$00");
-            System.out.print("\t\t" + prod.getQtdade() + "\n");
-        }
-
-        System.out.print("[0]\t\tSAIR DA APLICACAO\n"
-                + "--------------------------------------------------------------\n"
-                + " >> ");
-
-        int opcao = ler.nextInt();
-
-        if (opcao != 0) {
-            comprar(opcao);
-        } else {
-            System.out.println("Adeus e obrigado! ");
-            System.exit(0);
-        }
-    }
-
-    // menu administrador
-    private static void menuAdministrador() {
-
-    }
-
-    private static void comprar(int opcao) {
-
-        Produto produto = new Produto(
-                produtos.get(opcao - 1).getDescrição(),
-                produtos.get(opcao - 1).getPreço(),
-                produtos.get(opcao - 1).getQtdade());
-
-        System.out.println("Produto escolhido: " + produto.getDescrição() + "\nPreço: " + produto.getPreço() + "$00");
-
-        System.out.print("Por favor, Introduza a moeda no valor correspondente na maquina\n >> ");
-        int valor = ler.nextInt();
-
-        while (valor < produto.getPreço()) {
-            System.out.print("Falta mais " + (produto.getPreço() - valor) + "$00"
-                    + "Por favor, Introduza a moeda no valor correspondente na maquina\n>");
-            valor += ler.nextInt();
-        }
-
-        if (valor == produto.getPreço()) {
-            System.out.println("Total inserido: " + valor + "\n");
-        }
-
-        else if (valor > produto.getPreço()) {
-            int troco = valor - produto.getPreço();
-            System.out.println("Total inserido: " + valor + "\n"
-                    + "Troco: " + troco + "$00");
-            darTroco(troco, produto.getPreço());
-        }
-
-        System.out.println("Retire o seu produto. Obrigado");
-
-        produto.setQtdade(produto.getQtdade() - 1);
-        produtos.set(opcao - 1, produto);
-
-        new Ficheiros().escreverFile(produtos, txtProdutos);
-    }
-
-    private static void darTroco(int troco, int preço) {
-
-        do {
-            if (troco >= 100) {
-                for (Moedas moeda : moedas) {
-                    if (moeda.getValor() == 100) {
-                        moeda.setQtdade(moeda.getQtdade() - 1);
-                        int index = moedas.indexOf(moeda);
-                        moedas.set(index, moeda);
-                        troco -= 100;
-                    }
-                }
-            } else if (troco >= 50) {
-                for (Moedas moeda : moedas) {
-                    if (moeda.getValor() == 50) {
-                        moeda.setQtdade(moeda.getQtdade() - 1);
-                        int index = moedas.indexOf(moeda);
-                        moedas.set(index, moeda);
-                        troco -= 50;
-                    }
-                }
-            } else if (troco >= 20) {
-                for (Moedas moeda : moedas) {
-                    if (moeda.getValor() == 20) {
-                        moeda.setQtdade(moeda.getQtdade() - 1);
-                        int index = moedas.indexOf(moeda);
-                        moedas.set(index, moeda);
-                        troco -= 20;
-                    }
-                }
-            } else if (troco >= 10) {
-                for (Moedas moeda : moedas) {
-                    if (moeda.getValor() == 10) {
-                        moeda.setQtdade(moeda.getQtdade() - 1);
-                        int index = moedas.indexOf(moeda);
-                        moedas.set(index, moeda);
-                        troco -= 10;
-                    }
-                }
-            } else {
-                for (Moedas moeda : moedas) {
-                    if (moeda.getValor() == 5) {
-                        moeda.setQtdade(moeda.getQtdade() - 1);
-                        int index = moedas.indexOf(moeda);
-                        moedas.set(index, moeda);
-                        troco -= 5;
-                    }
-                }
-            }
-        } while (troco > preço);
-    }
-
-    // função para inicializar dados dos produtos e das moedas por padrão
-    private static void initializeProdCoins() {
-
-        // carregar dados dos ficheiros
-        produtos.add(new Produto("Cola", 80, 9));
-        produtos.add(new Produto("Fanta", 70, 7));
-        produtos.add(new Produto("Sprit", 60, 8));
-        produtos.add(new Produto("Sumol", 80, 6));
-
-        moedas.add(new Moedas(5, 10));
-        moedas.add(new Moedas(10, 10));
-        moedas.add(new Moedas(20, 10));
-        moedas.add(new Moedas(50, 10));
-        moedas.add(new Moedas(100, 10));
-
-    }
 }
